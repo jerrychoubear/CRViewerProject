ff180ea2e886b890f7d631dced0a99e675f9c2c8
JJ <jerrychoubear@gmail.com>
2017-06-06 01:44:24 +0800

    加入CRViewer.aspx。

---
+++ CRViewerProject/CRViewer.aspx
@@ -0,0 +1,21 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="CRViewer.aspx.cs" Inherits="CRViewerProject.CRViewer" %>
+
+<%@ Register assembly="CrystalDecisions.Web, Version=13.0.2000.0, Culture=neutral, PublicKeyToken=692fbea5521e1304" namespace="CrystalDecisions.Web" tagprefix="CR" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8"/>
+    <title></title>
+</head>
+<body>
+    <form id="form1" runat="server">
+    <div>
+    
+        <CR:CrystalReportViewer ID="MyViewer" runat="server" AutoDataBind="true" EnableDatabaseLogonPrompt="False" EnableParameterPrompt="False" ReuseParameterValuesOnRefresh="True" ToolPanelView="None" />
+    
+    </div>
+    </form>
+</body>
+</html>
---
+++ CRViewerProject/CRViewer.aspx.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using CrystalDecisions.CrystalReports.Engine;
+using CrystalDecisions.Shared;
+using CREngine = CrystalDecisions.CrystalReports.Engine;
+
+namespace CRViewerProject
+{
+    public partial class CRViewer : System.Web.UI.Page
+    {
+        private string FILE_PATH = @"CR.rpt";
+        private string SEQ = "5";
+
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            ReportDocument report = new ReportDocument();
+            ConnectionInfo info = new ConnectionInfo
+            {
+                AllowCustomConnection = true,
+                ServerName = @"MyOrcl",
+                UserID = "systaem",
+                Password = "orcl"
+            };
+            report.Load(FILE_PATH);
+            report.SetParameterValue("SEQ", SEQ);
+            foreach (CREngine.Table table in report.Database.Tables)
+            {
+                TableLogOnInfo log_info = table.LogOnInfo;
+                log_info.ConnectionInfo = info;
+                table.ApplyLogOnInfo(log_info);
+            }
+            MyViewer.ReportSource = report;
+        }
+    }
+}
